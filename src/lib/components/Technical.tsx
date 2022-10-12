ffdbe3cfc5bf7292e6a413a37877246ce78832a9
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2022-10-12 18:28:31 +0700

    refactor: :recycle: make some minor changes

--- src/lib/components/Technical.tsx
+++ src/lib/components/Technical.tsx
@@ -2,7 +2,7 @@ const Technical = () => {
   return (
     <div className="p-4">
       <h1 className="my-4 text-xl font-bold lg:text-2xl">Technical Skills:</h1>
-      <div className="flex max-w-[900px] flex-wrap justify-center gap-6 lg:justify-start">
+      <div className="flex max-w-[600px] flex-wrap justify-center gap-6 lg:justify-start">
         <div className="card-compact min-w-[150px] rounded-md bg-accent  shadow-xl">
           <div className="card-body">
             <h2 className="card-title text-center">HTML5, CSS3</h2>
